2eb85460c119421ca078a9df723f089038465fb3
priyanka897-star <priyankamore8997@gmail.com>
2021-09-22 22:20:53 +0530

    Added:Addition

---
+++ src/JavaPractice/Addition.java
@@ -0,0 +1,10 @@
+package JavaPractice;
+
+public class Addition {
+public static void main(String[] args) {
+	int num1 = 23;
+	int num2 =56;
+	int sum = num1 + num2;
+	System.out.println("Addition:"+sum);
+}
+}
