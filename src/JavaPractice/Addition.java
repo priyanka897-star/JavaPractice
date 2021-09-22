752a0cf5a3db4b6ef9c9029d5bfe693c35619daa
priyanka897-star <priyankamore8997@gmail.com>
2021-09-22 22:47:32 +0530

    Refactored:Values of num1 and num2

--- src/JavaPractice/Addition.java
+++ src/JavaPractice/Addition.java
@@ -2,9 +2,9 @@ package JavaPractice;
 
 public class Addition {
 public static void main(String[] args) {
-	int num1 = 23;
-	int num2 =56;
+	int num1 = 45;
+	int num2 =67;
 	int sum = num1 + num2;
-	System.out.println("Addition:"+sum);
+	System.out.println("Addition of num1 and num2:"+sum);
 }
 }
